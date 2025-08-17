5fb303ea40ebacb77eaad7929654990598bcbde9
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-08-18 01:05:05 +0200

    break word bugs are corrected

--- lt/lt139.py
+++ lt/lt139.py
@@ -10,29 +10,33 @@ def mark_word(s: str, wordDict: list[str]):
     segs_ = {word_: i for i, word_ in enumerate(wordDict)}
 
     for word_ in wordDict:
-        start_idx = s.find(word_)
-        if start_idx != -1:
+        start_idx = 0
+        while True:
+            start_idx = s.find(word_, start_idx)
+            if start_idx == -1:
+                break
             for i_ in range(start_idx, start_idx + len(word_)):
                 curr_seg_ = segs_[word_]
                 dict_[i_].add(curr_seg_)
+            start_idx += 1
 
     return dict_, segs_
+
 
 #|1111####
 #|||||||||
 #|####2222
 #|#####333
 
-def traverse_segments(dict_, segs_):
-
+def traverse_segments(s, dict_, segs_, wordDict):
     list_ = list([(0, segm_) for segm_ in dict_[0]])
 
     while list_:
         curr_letter_idx_, curr_segm_ = list_.pop()
-        shift_ = len(segs_[curr_segm_])
+        shift_ = len(wordDict[curr_segm_])
         new_letter_idx_ = shift_ + curr_letter_idx_
 
-        if new_letter_idx_ == len(segs_) + 1:
+        if new_letter_idx_ == len(s):
             return True
 
         for segm_ in dict_[new_letter_idx_]:
@@ -41,17 +45,10 @@ def traverse_segments(dict_, segs_):
     return False
 
 
-
-
-if __name__ == "main.py":
-    s = "leetcode"
+if __name__ == "__main__":
+    s = "leetcodeleet"
 
     wordDict = ["leet", "code"]
 
     dict_, segs_ = mark_word(s, wordDict)
-    print(traverse_segments(dict_, segs_))
-
-
-
-
-
+    print(traverse_segments(s, dict_, segs_, wordDict))
