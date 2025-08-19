eda238bd024a02b4e6f727e2ee6b14101ea63004
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-08-19 08:42:19 +0200

    pre soltuion to lt 42

--- lt/lt42__.py
+++ lt/lt42__.py
@@ -1,28 +1,14 @@
-height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-# height = [4, 2, 0, 3, 2, 5]
+height = [4, 2, 0, 3, 2, 5]
 
 # ##  #
 # ######## #
 ############
 
-def find_left_border(heights, start_idx):
-    for i in range(start_idx, len(heights)-2):
-        if heights[i+1] < heights[i]:
-            return i
-
-    return -1
-
-def find_right_border(heights, start_idx):
-    for i in range(start_idx, len(heights)-1):
-        if heights[i+1] > heights[i]:
-            return i+1
-
-    return -1
 
 def find_border(heights, start_idx):
     for i in range(start_idx, len(heights)-1):
-        if heights[i+1] > heights[i]:
-            return i+1
+        if heights[i+1] < heights[i] and heights[i] > heights[i-1]:
+            return i
 
     return -1
 
@@ -34,47 +20,49 @@ def fill_capable(heights, left_border_idx, right_border_idx):
 
     return filled_
 
+#  #
+## ##
+#####
+
+def pick_global(heights, borders):
+
+    peak_heights = heights[borders]
+
+    i, j = 0, 0
+    global_heights = heights[borders]
+
+
+    while i < len(peak_heights):
+        if peak_heights[i] < global_heights[i+1]:
+            i += 1
+        else:
+
+
 
 
 
 
 def fill_water(heights):
 
-    i, j = 0, 0
-    capable_ = False
-    pre_capable_ = False
+    left_border_idx_ = 1
 
+    heights = [0, *heights]
+    borders = []
     filled_ = 0
-    capacity_ = 0
-
-    len_ = len(heights)
-
-    left_border_idx_ = 0
-    right_border_idx_ = 1
 
     while True:
-        if not pre_capable_:
-            left_border_idx_ = find_left_border(heights, left_border_idx_)
+        border = find_border(heights, left_border_idx_)
+        if border == -1:
+            break
+        else:
+            left_border_idx_ = border + 1
+            borders.append(border)
 
-            if left_border_idx_ != -1:
-                pre_capable_ = True
-            else:
-                return filled_
+    for i_ in range(len(borders)-1):
+        filled_ += fill_capable(heights, borders[i_], borders[i_+1])
 
-        if pre_capable_:
-            right_border_idx_ = find_left_border(heights, right_border_idx_)
-            if right_border_idx_ != -1:
-                capable_ = True
-            else:
-                return filled_
 
-        if capable_:
-            filled_ += fill_capable(heights, left_border_idx_, right_border_idx_)
-            pre_capable_ = False
-            capable_ = False
-
-            left_border_idx_ = right_border_idx_
-            right_border_idx_ += 1
+    return filled_
 
 
 
