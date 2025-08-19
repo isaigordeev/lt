d59f32ced9c550617158315006bed8f5efc9e60e
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-08-19 16:30:34 +0200

    many many edge cases

--- lt/lt42__.py
+++ lt/lt42__.py
@@ -1,12 +1,12 @@
 # ##  #
 # ######## #
 ############
-height = [0,1,0,2,1,0,1,3,2,1,2,1]
+height = [9,6,8,8,5,6,3]
 
 
 def find_border(heights, start_idx):
     for i in range(start_idx, len(heights)-1):
-        if heights[i+1] < heights[i] and heights[i] > heights[i-1]:
+        if heights[i+1] < heights[i] and heights[i] >= heights[i-1]:
             return i
 
     return -1
@@ -14,8 +14,8 @@ def find_border(heights, start_idx):
 def fill_capable(heights, left_border_idx, right_border_idx):
     capacity_ = min(heights[left_border_idx], heights[right_border_idx])
     filled_ = 0
-    for i_ in range(left_border_idx, right_border_idx):
-        filled_ += ( capacity_ - heights[i_] )
+    for i_ in range(left_border_idx+1, right_border_idx):
+        filled_ += max( capacity_ - heights[i_] , 0)
 
     return filled_
 
@@ -39,8 +39,15 @@ def pick_global(heights, borders):
         peak_j = peak_heights[peak_heights_[j]]
         if peak_i > peak_j:
             j += 1
+            if j == len(peak_heights_):
+                global_peaks += [peak_heights_[j-1]]
         else:
-            global_peaks += [peak_heights_[i], peak_heights_[j]]
+            if not global_peaks or global_peaks[-1] != peak_heights_[i]:
+                global_peaks += [peak_heights_[i], peak_heights_[j]]
+            else:
+                global_peaks += [peak_heights_[j]]
+
+
             i = j
             j += 1
 
@@ -63,7 +70,9 @@ def fill_water(heights):
             left_border_idx_ = border + 1
             borders.append(border)
 
-    borders = pick_global(heights, borders)
+    if len(borders) > 2:
+        borders = pick_global(heights, borders)
+
 
     for i_ in range(len(borders)-1):
         filled_ += fill_capable(heights, borders[i_], borders[i_+1])
