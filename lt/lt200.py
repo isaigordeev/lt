e8c33cae02dbd21fe8193f33492b101fe4c04c48
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-11-24 00:55:01 +0100

    uf for lt200

--- lt/lt200.py
+++ lt/lt200.py
@@ -31,57 +31,75 @@
 # - grid[i][j] is '0' or '1'
 
 
-def create_uf(n):
-    return [i for i in range(n)]
+class UnionFind:
+    def __init__(self, grid):
+        # suppose valid
+        self.N = len(grid)
+        self.M = len(grid[0])
+
+        self.create_uf()
+
+    @staticmethod
+    def find(uf, i):
+        _curr = i
+        while _curr != uf[i]:
+            _curr = uf[i]
+
+        return _curr
+
+    @staticmethod
+    def union(uf, i, j):
+        uf[i] = uf[j]
+
+    def idx(self, i, j):
+        return i * self.M + j
+
+    def create_uf(self):
+        self.uf = [i for i in range(self.N * self.M)]
+
+        return self.uf
+
+    def print_(self):
+        for i in range(self.N):
+            print(self.uf[self.M * i : self.M * (i + 1)])
 
 
-def find(uf, i):
-    _curr = i
-    while _curr != uf[i]:
-        _curr = uf[i]
+def search_for_group(grid, i, j, uf) -> int:
+    n = len(grid)
+    m = len(grid[0])
 
-    return _curr
+    _group = i * m + j
 
-
-def union(uf, i, j):
-    uf[i] = uf[j]
-
-
-def search_for_union(grid, i, j):
     moves = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
     for _move in moves:
         delta_i, delta_j = _move
 
-        if i + delta_i < len(grid) and j + delta_j < len(grid[0]):
-            group = grid[i + delta_i][j + delta_j]
-        else:
-            group = 0  # water outside the grid
+        if i + delta_i < len(grid) and j + delta_j < len(grid[0]):  # borders
+            if grid[i + delta_i][j + delta_j] == 1:  # if island
+                _group = min(_group, uf[(i + delta_i) * m + j + delta_j])
 
-    return group
+    return _group
 
 
 def numIslands_uf(grid):
-    m = len(grid)
-    n = len(grid[0])
+    n = len(grid)
+    m = len(grid[0])
 
-    _uf = create_uf(n * m)
+    _uf = UnionFind(grid=grid)
 
     _visited = [False for _ in range(n * m)]
-
-    moves = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
     for i, _x in enumerate(grid):
         for j, _y in enumerate(_x):
             if grid[i][j] == 1:
-                _cur_idx_uf = m * i + j
-                print("cur", _cur_idx_uf)
-                for _move in moves:
-                    delta_i, delta_j = _move
-                    if _visited[(i + delta_i) * m + j + delta_j]:
-                        continue
-
-                    if i + delta_i < len(grid) and j + delta_j < len(grid[0]):
-                        _uf[(i + delta_i) * m + j + delta_j] = _cur_idx_uf
+                _group = search_for_group(grid, i, j, _uf.uf)
+                _uf.uf[_uf.idx(i, j)] = _group
+            else:  #  water
+                _uf.uf[_uf.idx(i, j)] = -1
 
     return _uf
+
+
+# 0 1 2 3 4
+# 5 6 7 8 9
--- tests/test_lt200.py
+++ tests/test_lt200.py
@@ -1,13 +1,25 @@
-from lt.lt200 import numIslands_uf
+from lt.lt200 import UnionFind, numIslands_uf
+
+# grid = [
+#     [1, 1, 1, 1, 0],
+#     [1, 1, 0, 1, 0],
+#     [1, 1, 0, 0, 0],
+#     [0, 0, 0, 0, 0],
+# ]
+
+
+# uf: UnionFind = numIslands_uf(grid)
+# uf.print_()
 
 grid = [
-    ["1", "1", "1", "1", "0"],
-    ["1", "1", "0", "1", "0"],
-    ["1", "1", "0", "0", "0"],
-    ["0", "0", "0", "0", "0"],
+    [1, 1, 0, 0, 0],
+    [1, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1],
 ]
 
 
-uf = numIslands_uf(grid)
+uf: UnionFind = numIslands_uf(grid)
+uf.print_()
 
-print(uf)
+print("islands: ", len(set(uf.uf)) - 1)
